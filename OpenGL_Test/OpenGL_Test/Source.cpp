8d49acdaf9432bfc0be236d7dd24ff563d09a3e1
argir-boyadzhiev <56366942+argir-boyadzhiev@users.noreply.github.com>
2020-07-17 11:03:51 +0300

    reworked

--- OpenGL_Test/OpenGL_Test/Source.cpp
+++ OpenGL_Test/OpenGL_Test/Source.cpp
@@ -2,57 +2,78 @@
 #include <glad/glad.h> 
 #include <GLFW\glfw3.h>
 
+void initialize();
+bool createWindow();
+bool setUpGLAD();
+void mainLoop();
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+GLFWwindow* window;
+
 
 int main()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
-
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL-Test", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
+    initialize();
+    if (!createWindow()) return -1;
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+    if (!setUpGLAD()) return -1;
 
     glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
-    
-
     while (!glfwWindowShouldClose(window))
     {
-        // input
-        processInput(window);
-
-        // rendering commands here
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // check and call events and swap the buffers
-        glfwPollEvents();
-        glfwSwapBuffers(window);
+        mainLoop();
     }
 
     glfwTerminate();
     return 0;
+}
+
+void initialize() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+bool createWindow() {
+    window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL-Test", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return false;
+    }
+    else return true;
+}
+
+bool setUpGLAD() {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return false;
+    }
+    else return true;
+}
+
+void mainLoop() {
+    // input
+    processInput(window);
+
+    // rendering commands here
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // check and call events and swap the buffers
+    glfwPollEvents();
+    glfwSwapBuffers(window);
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
