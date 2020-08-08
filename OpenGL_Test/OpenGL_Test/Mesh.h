8977731d0d792edd23787a9c4a7258fe64cc1d8d
argir-boyadzhiev <56366942+argir-boyadzhiev@users.noreply.github.com>
2020-08-08 20:50:24 +0300

    mesh

---
+++ OpenGL_Test/OpenGL_Test/Mesh.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <glad/glad.h> // holds all OpenGL type declarations
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "Shader.h"
+
+#include <string>
+#include <vector>
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;
+};
+
+class Mesh {
+public:
+    // mesh data
+    std::vector<Vertex>       vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture>      textures;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+    void Draw(Shader& shader);
+private:
+    //  render data
+    unsigned int VAO, VBO, EBO;
+
+    void setupMesh();
+};
+
